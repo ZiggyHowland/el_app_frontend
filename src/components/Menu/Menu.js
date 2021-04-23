8fce530856c9efb669151eb29e39d3c720cc7ba1
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-23 11:59:50 +0200

    Message

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -15,7 +15,7 @@ export default function Menu(props) {
 			<Nav
 				className="mr-auto"
 				variant="pills"
-				activeKey={window.location.pathname}
+				//activeKey={window.location.pathname}
 				// defaultActiveKey="/"				
 			>
 			
