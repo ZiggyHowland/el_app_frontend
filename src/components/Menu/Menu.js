bc8af802a28902c5a4ac9f7567845b80400393b1
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-23 12:41:35 +0200

    Testet window-activeKey without success

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -15,6 +15,7 @@ export default function Menu(props) {
 			<Nav
 				className="mr-auto"
 				variant="pills"
+				
 				//activeKey={window.location.pathname}
 				// defaultActiveKey="/"				
 			>
